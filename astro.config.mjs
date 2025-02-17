1efe788d6779d8578d96938e94ee8be87e50b866
hhouaiss <hassan.houaiss@gmail.com>
2025-02-17 16:54:53 +0100

    fixing vercel error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static',
-  adapter: vercel({
-    analytics: true,
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'server',
+  adapter: vercel(),
 });
