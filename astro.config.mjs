b95fcba3b23ba62a2f7afa8a2d8e7a8fb47fbbb4
hhouaiss <hassan.houaiss@gmail.com>
2025-02-17 16:50:37 +0100

    server side fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercel({
+    analytics: true,
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
